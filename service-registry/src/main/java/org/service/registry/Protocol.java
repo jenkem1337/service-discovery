2d15a1e88a02ea2a90fe57fc029584df28a35e6b
Hasancan Sahan <yamahasirossi@gmail.com>
2026-01-31 22:48:17 +0300

    Protocol enum more generic at the moment

--- service-registry/src/main/java/org/service/registry/Protocol.java
+++ service-registry/src/main/java/org/service/registry/Protocol.java
@@ -1,8 +1,29 @@
 package org.service.registry;
 
 public enum Protocol {
-    HTTP,
-    GRPC,
-    TCP,
-    UDP
-}
+    HTTP("http"),
+    GRPC("grpc"),
+    TCP("tcp"),
+    UDP("udp");
+
+    private final String value;
+
+    Protocol(String value) {
+        this.value = value;
+    }
+
+    public static Protocol from(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("Protocol cannot be null");
+        }
+
+        for (Protocol protocol : values()) {
+            if (protocol.value.equalsIgnoreCase(input)) {
+                return protocol;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown protocol: " + input);
+    }
+
+    }
