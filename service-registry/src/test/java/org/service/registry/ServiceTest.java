45f2193c19fcc8612f6425a5bc872ee02c947ac8
Hasancan Sahan <yamahasirossi@gmail.com>
2026-01-31 22:49:12 +0300

    Update ServiceTest.java

--- service-registry/src/test/java/org/service/registry/ServiceTest.java
+++ service-registry/src/test/java/org/service/registry/ServiceTest.java
@@ -12,13 +12,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class ServiceTest {
     @Test
     void createServiceObject() {
-        var service = Service.withoutHeartbeatMessage(
+        var service = Service.of(
                 Generators.timeBasedEpochGenerator().generate(),
                 Generators.timeBasedEpochGenerator().generate(),
                 Name.of("service-1"),
                 IpAddress.of("123.123.123.123"),
                 Port.of(3434),
-                Protocol.HTTP,
+                Protocol.from("http"),
                 Instant.now());
         assertNotNull(service);
     }
